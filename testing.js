92012b75e99ebe4177d3fa355ee090100ea5f479
rabinlc <42234876+rabinlc@users.noreply.github.com>
2020-08-03 15:20:40 +0545

    Update testing.js

--- testing.js
+++ testing.js
@@ -1,12 +1,22 @@
 var x = 1
-var speed = 0.01
 function setup(){
   createSketch(450, 400)
 }
 
 function draw(){
 	background(51)
+	rotate(x)
+	// style inside push and pop doesnot effect outside style
+	push()
+	fill(0)
+	rect(100,100,50,50)
+	ellipse(110,10,50,50)
+	pop()
 	fill(150)
 	rect(100,100,50,50)
-	line(100,100,50,50)
+	ellipse(110,10,50,50)
+	x++;
+	if (x > 100){
+	  noLoop()
+	}
 }
